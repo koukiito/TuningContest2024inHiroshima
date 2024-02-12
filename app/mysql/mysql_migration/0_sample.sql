ee80aff82b816c6a07dbb94ab8812ae9672cce98
koukiito <dev.koukiito@gmail.com>
2024-02-12 20:14:57 +0900

    fix: migration sql

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,4 @@
--- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+USE `app`;
+ALTER table `user` ADD INDEX `idx_user_1`(`entry_date`, `kana`);
+ALTER table `user` ADD INDEX `idx_user_2`(`mail`, `password`);
+ALTER table `department_role_member` ADD INDEX `idx_dept_r_m_1`(`user_id`, `belong`);
