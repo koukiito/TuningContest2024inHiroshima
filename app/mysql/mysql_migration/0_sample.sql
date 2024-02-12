be29c5fa6c396db6a162b0c4b158371a13d696d4
koukiito <dev.koukiito@gmail.com>
2024-02-12 19:59:12 +0900

    add: migration sql

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,3 +1,4 @@
 USE `app`;
 ALTER table `user` ADD INDEX `idx_user_1`(`entry_date`, `kana`);
 ALTER table `user` ADD INDEX `idx_user_2`(`mail`, `password`);
+ALTER table `department_role_member` ADD INDEX `idx_dept_r_m_1`(`user_id`, `belong`);
