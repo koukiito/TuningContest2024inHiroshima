3ef7c516f51c8f8659d07d2c6c323bf2e4fcfb12
koukiito <dev.koukiito@gmail.com>
2024-02-13 13:09:53 +0900

    fix: optimize query

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,6 +1,7 @@
 USE `app`;
 ALTER table `user` ADD INDEX `idx_user_1`(`entry_date`, `kana`);
 ALTER table `user` ADD INDEX `idx_user_2`(`mail`, `password`);
+--ALTER table `department_role_member` ADD INDEX `idx_dept_r_m_1`(`user_id`, `belong`);
 
 CREATE TABLE `file_resized` (
     `file_id` VARCHAR(36) NOT NULL,
